acd7a0ce7ae15b0b8d083bab8d28d6cd00018729
amorre92 <amorre92@gmail.com>
2022-03-10 10:35:33 +0100

    add all scores

--- App.js
+++ App.js
@@ -4,21 +4,26 @@ import Score from './src/components/Score';
 import AirClassique from './src/scores/AirClassique';
 import Ambulant from './src/scores/Ambulant';
 import Arlequin from './src/scores/Arlequin';
+import Brigands from './src/scores/Brigands';
 import Cavalcade from './src/scores/Cavalcade';
+import Chasseurs from './src/scores/Chasseurs';
 import Classe from './src/scores/Classe';
 import DginsDeLestene from './src/scores/DginsDeLestene';
 import Doudou from './src/scores/Doudou';
 import EloiACharleroi from './src/scores/EloiACharleroi';
 import JuifErrant from './src/scores/JuifErrant';
 import LionDeBelgique from './src/scores/LionDeBelgique';
+import Marins from './src/scores/Marins';
 import MereTantPis from './src/scores/MereTantPis';
 import PasDeCharge from './src/scores/PasDeCharge';
 import Paysan from './src/scores/Paysan';
 import PetitJeuneHommeDeBinche from './src/scores/PetitJeuneHommeDeBinche';
+import PolkaMarche from './src/scores/PolkaMarche';
 import PostillonDeLongjumeau from './src/scores/PostillonDeLongjumeau';
 import QuandMgrandMere from './src/scores/QuandMgrandMere';
 import SansSouci from './src/scores/SansSouci';
 import Serenade from './src/scores/Serenade';
+import TrompetteDesCentGardes from './src/scores/TrompetteDesCentGardes';
 import ViventLesBleus from './src/scores/ViventLesBleus';
 import VosArezInAubade from './src/scores/VosArezInAubade';
 
@@ -44,6 +49,11 @@ export default function App() {
   const dginsDeLestene = DginsDeLestene();
   const doudou = Doudou();
   const quandMgrandMere= QuandMgrandMere();
+  const chasseurs = Chasseurs();
+  const trompetteDesCentGardes = TrompetteDesCentGardes();
+  const marins = Marins();
+  const brigands = Brigands();
+  const polkaMarche = PolkaMarche();
 
   const data = [
     {key: airClassique.id, value: airClassique}, 
@@ -65,7 +75,13 @@ export default function App() {
     {key: arlequin.id, value: arlequin},
     {key: dginsDeLestene.id, value: dginsDeLestene},
     {key: doudou.id, value: doudou},
-    {key: quandMgrandMere.id, value: quandMgrandMere}
+    {key: quandMgrandMere.id, value: quandMgrandMere},
+    {key: chasseurs.id, value: chasseurs},
+    {key: trompetteDesCentGardes.id, value: trompetteDesCentGardes},
+    {key: marins.id, value: marins},
+    {key: brigands.id, value: brigands},
+    {key: polkaMarche.id, value: polkaMarche}
+    
   ]
 
   return (
